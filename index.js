e96c4f8c3ef0ff7a6587e0a7c89f89010a9bab1f
Almaz <almazdd@gmail.com>
2021-12-22 16:34:31 +0600

    nodejs CRUD

--- index.js
+++ index.js
@@ -1,84 +1,21 @@
-const fs = require('fs');
-const { resolve } = require('path');
-const path = require('path')
+import express from 'express'
+import mongoose from 'mongoose'
+import router from './router.js'
 
-// fs.mkdir(path.resolve(__dirname, 'dir'), (err) =>{
-//     if(err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log('folder created');
-// })
+const PORT = 5002
+const DB_URL = 'mongodb+srv://admin:admin123@cluster0.qmrfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
+const app = express()
+app.use(express.json())
+app.use('/api', router)
 
-// fs.rmdir(path.resolve(__dirname, 'dir'), (err) =>{
-//     if(err){
-//         throw err;
-//     }
-//     console.log('folder deleted');
-// })
-
-// fs.writeFile(path.resolve(__dirname, 'test.txt'), 'hi there - ', (err) =>{
-//     if(err){
-//         throw err;
-//     }
-//     fs.appendFile(path(__dirname, 'test.txt'), 'last added', (err) =>{
-//         if(err){
-//             throw err;
-//         }
-//         console.log('file added');
-//     })
-// })
-
-function writeFile(path, data){
-    return new Promise((res, rej) => fs.writeFile(path, data, (err) => {
-        if (err){
-            return rej(err.message)
-        }
-        res()
-    }))
+async function startApp () {
+  try {
+    await mongoose.connect(DB_URL)
+    app.listen(PORT, () => console.log('Server started on port: ' + PORT))
+  } catch (e) {
+    console.log(e)
+  }
 }
 
-function appendFile (path, data)  {
-    return new Promise((res, rej) => fs.appendFile(path, data, (err) => {
-        if (err){
-            return rej(err.message)
-        }
-        res()
-    }))
-}
-
-function readFileAs(path)  {
-    return new Promise((res, rej) => fs.readFile(path, {encoding: 'utf-8'}, (err, data) => {
-        if (err){
-            return rej(err.message)
-        }
-        res(data)
-    }))
-}    
-
-function ramoveFile(path)  {
-    return new Promise((res, rej) => fs.rm(path, (err) => {
-        if (err){
-            return rej(err.message)
-        }
-        res()
-    }))
-}    
-
-// ramoveFile(path.resolve(__dirname, 'test.txt'))
-//     .then(()=> console.log('file deleted'))
-
-// writeFile(path.resolve(__dirname, 'test.txt'), 'data')
-//     .then(()=> appendFile(path.resolve(__dirname, 'test.txt'), '123'))
-//     .then(()=> readFileAs(path.resolve(__dirname, 'test.txt')))
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
-
-   
-const text = process.env.TEXT || '';
-
-writeFile(path.resolve(__dirname, 'test.txt'), text)
-    .then(() => readFileAs(path.resolve(__dirname, 'test.txt')))
-    .then(data => data.split(' ').length)
-    .then(count => writeFile(path.resolve(__dirname, 'count.txt'), data))
+startApp()
